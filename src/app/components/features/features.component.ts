955ed23cf3625b0b5ba348630f441ec967c8294c
josedanielcr <jdcanales1905@gmail.com>
2025-05-13 19:55:45 -0600

    feat: pulse effect on feature button cards

--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, Component, OnChanges, SimpleChanges } from '@angular/core';
 import {NgClass} from '@angular/common';
 
 @Component({
